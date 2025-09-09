b3e380b5f2de653cd2cc4c1fff1cad353536a038
ElviraGiniyatullina <elvi22@mail.ru>
2025-09-09 22:42:12 +0500

    grocery store changes with the addition 14 files

---
+++ Hw20/1/blog/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100, verbose_name="Заголовок")
+    slug = models.CharField(max_length=150, verbose_name="slug", null=True, blank=True)
+    body = models.TextField(verbose_name="Содержимое")
+    photo = models.ImageField(
+        upload_to="blog/photo",
+        null=True,
+        blank=True,
+        verbose_name="Фото",
+        help_text="Загрузите фото по теме",
+    )
+    date_create = models.DateField(
+        null=True,
+        blank=True,
+        verbose_name="Дата создания",
+        help_text="Укажите дату создания",
+    )
+    is_published = models.BooleanField(default=True, verbose_name="Опубликовано")
+    views_count = models.IntegerField(default=0, verbose_name="Просмотры")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
